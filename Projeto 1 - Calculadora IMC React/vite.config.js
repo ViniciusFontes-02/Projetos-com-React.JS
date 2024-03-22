40cb380615139b2e7758770af470dd7ee108998e
Vinicius Fontes <vinif.santos2002@gmail.com>
2024-03-22 16:17:06 -0300

    configurações de deploy

--- Projeto 1 - Calculadora IMC React/vite.config.js
+++ Projeto 1 - Calculadora IMC React/vite.config.js
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  base: "/Projetos-com-React.JS/tree/main/Projeto%201%20-%20Calculadora%20IMC%20React",
+});
